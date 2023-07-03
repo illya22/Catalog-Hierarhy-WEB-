264e5990bbec1e4457ed6146654e2c12003ce497
ilgyha228 <ilgyha228@gmail.com>
2023-07-03 22:19:44 +0300

    adding view model

---
+++ Catalog Hierarhy/Models/CatalogViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Catalog_Hierarhy.Models
+{
+    public class CatalogViewModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int? ParentId { get; set; }
+        public List<Catalog>? Children { get; set; }
+    }
+}
